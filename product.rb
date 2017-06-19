da05dc40e07248776851036c6a8d2db035cbdd4a
Danira Karovic <karovic.danira@gmail.com>
2017-06-19 10:03:51 +0200

    Update product.rb
    product file updated

--- product.rb
+++ product.rb
@@ -1,11 +1,9 @@
 class Product
-attr_accessor :model, :price, :in_stock
+  attr_accessor :model, :price, :in_stock
+  
   def initialize(model, price, in_stock)
     @model = model
     @price = price
     @in_stock = in_stock
-  end
-  def to s
-    "#{model}, #{price}, #{in_stock}"
   end
 end
